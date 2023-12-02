d5b959d8bba3b38e953767fa3294f81406d52410
Ppolyak <pasha_polyakov8@mail.ru>
2023-12-02 21:20:38 +0300

    ref

--- server-with-basic-routing/Server/server.go
+++ server-with-basic-routing/Server/server.go
@@ -10,13 +10,16 @@ import (
 	time2 "time"
 )
 
+func main() {
+	startServer()
+}
+
 func startServer() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/time", showTime)
 	log.Println("Запуск веб-сервера на http://127.0.0.1:7575")
 	err := http.ListenAndServe(":7575", mux)
-	getHomePage()
 	log.Fatal(err)
 }
 
